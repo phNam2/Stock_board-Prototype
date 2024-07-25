d2ea805ee98d12cecc5634558ee68aa77daf0b8c
nam <namhoangpham96@gmail.com>
2024-07-25 16:15:12 -0400

    Connect api app to the setting.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api",
+    "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
